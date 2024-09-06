6dc7c446523a4c1ac33bc3eb36983a182284d015
AdirthaBorgohain <adirthaborgohain@gmail.com>
2024-09-07 01:37:41 +0530

    removed references to configs yaml file

--- utils/__init__.py
+++ utils/__init__.py
@@ -5,7 +5,6 @@ from utils.helpers import *
 from dotenv import load_dotenv
 
 root_dir = Path(__file__).parent.absolute().parent
-base_dir = os.path.join(root_dir, "utils")
 print("root_dir: ", root_dir)
 
 pdf_dir = os.path.join(root_dir, "pdf")
@@ -16,18 +15,14 @@ os.makedirs(log_dir, exist_ok=True)
 
 load_dotenv()
 
-# Load configuration file
-config_path = os.getenv("config_path", default=os.path.join(base_dir, "configs.yaml"))
-configs = load_yaml_file(config_path)
-
 logger_handler = Logger(log_file_name="pdf_slack_bot.log", log_file_dir=log_dir)
 logger = logger_handler.create_time_rotating_log(when="day", backup_count=10, name="pdf_slack_bot")
 
-configs.update({
+configs = {
     'root_dir': root_dir,
     'pdf_dir': pdf_dir,
     'logger': logger
-})
+}
 
 configs = DotDict(configs)
 
